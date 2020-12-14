0ab822e547e1c892d6d1caa391420e28e487a002
eduuh <edwinkamaumuraya0@outlook.com>
2020-12-14 14:52:38 +0000

    mades some minor changes on the server

--- Application/User/User.cs
+++ Application/User/User.cs
@@ -5,6 +5,5 @@ namespace Application.User
         public string DispalyName { get; set; }
         public string Token { get; set; }
         public string Username { get; set; }
-        public string Image { get; set; }
     }
 }
